eb8bb698fd893f02e0c11cc581e9a8a22bdcac70
Tom Silver <tomssilver@gmail.com>
2024-08-09 14:37:49 -0400

    wip

--- pybullet-cup-manipulation/demo.py
+++ pybullet-cup-manipulation/demo.py
@@ -3,19 +3,21 @@
 from pybullet_helpers.robots import create_pybullet_robot
 from pybullet_helpers.robots.single_arm import SingleArmPyBulletRobot
 from pybullet_helpers.geometry import Pose, get_pose, multiply_poses
+from pybullet_helpers.joint import JointPositions
+from pybullet_helpers.ikfast.utils import ikfast_inverse_kinematics
 from pybullet_helpers.camera import create_gui_connection
 from pybullet_helpers.utils import create_pybullet_cylinder, create_pybullet_block
 from pybullet_helpers.motion_planning import run_motion_planning
 from pybullet_helpers.gui import visualize_pose
+from itertools import islice
 import numpy as np
-import time
 from pathlib import Path
 
 import pybullet as p
 
 
-def _initialize_scene() -> tuple[SingleArmPyBulletRobot, Pose, int, int, int]:
-    """Returns robot, cup ID, table ID, collision ID."""
+def _initialize_scene() -> tuple[SingleArmPyBulletRobot, Pose, int, int, set[int]]:
+    """Returns robot, cup ID, table ID, other collision IDs."""
 
     robot_base_pose = Pose((0.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0))
     robot_home_joints = [
@@ -35,10 +37,15 @@ def _initialize_scene() -> tuple[SingleArmPyBulletRobot, Pose, int, int, int]:
     robot_holder_position = (0.0, 0.0, -0.5)
     robot_holder_orientation = (0.0, 0.0, 0.0, 1.0)
 
-    collision_region_orientation = (0.0, 0.0, 0.0, 1.0)
-    collision_region_position = (-0.75, -0.3, 0.0)
-    collision_region_half_extents = (0.5, 0.75, 1.0)
-    collision_region_rgba = (1.0, 0.0, 0.0, 0.25)
+    collision_region1_orientation = (0.0, 0.0, 0.0, 1.0)
+    collision_region1_position = (-0.75, -0.3, 0.0)
+    collision_region1_half_extents = (0.5, 0.75, 1.0)
+    collision_region1_rgba = (1.0, 0.0, 0.0, 0.25)
+
+    collision_region2_orientation = (0.0, 0.0, 0.0, 1.0)
+    collision_region2_position = (0.075, 0.55, 0.05)
+    collision_region2_half_extents = (0.05, 0.05, 0.05)
+    collision_region2_rgba = (1.0, 0.0, 0.0, 0.25)
 
     wheelchair_position = (-0.5, 0.0, -0.25)
     wheelchair_orientation = (0.0, 0.0, 1.0, 0.0)
@@ -54,15 +61,7 @@ def _initialize_scene() -> tuple[SingleArmPyBulletRobot, Pose, int, int, int]:
     cup_position = (0.0, 0.75, cup_length / 2)
     cup_orientation = (0.0, 0.0, 0.0, 1.0)
 
-    # TODO remove
-    # Just above the table surface, pointing towards the cup from the robot.
-    # reference_position = np.add(table_position,
-    #                             (-table_half_extents[0], table_half_extents[1],
-    #                              table_half_extents[2] + 0.01))
-    # reference_orientation = (0.0, 0.0, 1.0, 0.0)
-    # reference_frame = Pose(reference_position, reference_orientation)
-
-    physics_client_id = create_gui_connection()
+    physics_client_id = create_gui_connection(camera_pitch=180)
     p.setGravity(0.0, 0.0, 0.0, physicsClientId=physics_client_id)
 
     # Create robot.
@@ -128,27 +127,107 @@ def _initialize_scene() -> tuple[SingleArmPyBulletRobot, Pose, int, int, int]:
         physicsClientId=physics_client_id,
     )
 
-    # Create collision area.
-    collision_region_id = create_pybullet_block(
-        collision_region_rgba,
-        half_extents=collision_region_half_extents,
+    # Create collision areas.
+    collision_region_ids = set()
+    
+    collision_region_id1 = create_pybullet_block(
+        collision_region1_rgba,
+        half_extents=collision_region1_half_extents,
         physics_client_id=physics_client_id,
     )
     p.resetBasePositionAndOrientation(
-        collision_region_id,
-        collision_region_position,
-        collision_region_orientation,
+        collision_region_id1,
+        collision_region1_position,
+        collision_region1_orientation,
         physicsClientId=physics_client_id,
     )
+    collision_region_ids.add(collision_region_id1)
 
-    return robot, cup_id, table_id, collision_region_id
+    collision_region_id2 = create_pybullet_block(
+        collision_region2_rgba,
+        half_extents=collision_region2_half_extents,
+        physics_client_id=physics_client_id,
+    )
+    p.resetBasePositionAndOrientation(
+        collision_region_id2,
+        collision_region2_position,
+        collision_region2_orientation,
+        physicsClientId=physics_client_id,
+    )
+    collision_region_ids.add(collision_region_id2)
+
+    return robot, cup_id, table_id, collision_region_ids
+
+
+def collision_free_ik(robot: SingleArmPyBulletRobot, end_effector_pose: Pose, collision_ids: set[int]) -> JointPositions | None:
+    """Find an IK solution that results in no collisions between the robot
+    and the collision_ids."""
+    
+    # TODO: move this function into pybullet-helpers.
+    # TODO move out these hyperparameters
+    # TODO handle no solutions possible
+    max_time: float = 0.05
+    max_attempts: int = 1000000000
+    max_candidates: int = 10
+    max_distance: float = np.inf
+    norm: float = np.inf
+    rng = np.random.default_rng(0)
+
+    physics_client_id = robot.physics_client_id
+
+    generator = ikfast_inverse_kinematics(robot, end_effector_pose, max_time, max_distance, max_attempts, norm, rng)
+    generator = islice(generator, max_candidates)
+
+    for candidate in generator:
+        # TODO refactor to avoid this...
+        first_finger_idx, second_finger_idx = sorted(
+            [robot.left_finger_joint_idx, robot.right_finger_joint_idx]
+        )
+        candidate.insert(first_finger_idx, robot.open_fingers)
+        candidate.insert(second_finger_idx, robot.open_fingers)
+
+        # TODO verify that candidate is successful (or is that already validated?)
+
+        robot.set_joints(candidate)
+
+        has_collision = False
+        p.performCollisionDetection(physicsClientId=physics_client_id)
+        for body in collision_ids:
+            if p.getContactPoints(
+                robot.robot_id, body, physicsClientId=physics_client_id
+            ):
+                has_collision = True
+                break
+        if not has_collision:
+            return candidate
+        
+    return None
+
+
+def _sample_grasp(cup_pose: Pose, rng: np.random.Generator) -> Pose:
+    # Pose of grasping the center of the cup.
+    default_cup_pregrasp_transform = Pose((0.0, 0.0, 0.0), (0, np.sqrt(2) / 2, np.sqrt(2) / 2, 0))
+    grasp = multiply_poses(cup_pose, default_cup_pregrasp_transform)
+
+    # Randomly rotate around the cup.
+    angle = rng.uniform(-np.pi / 4, np.pi / 4)
+    rotation_quat = p.getQuaternionFromEuler((0.0, angle, 0.0))
+    tf = Pose((0.0, 0.0, 0.0), rotation_quat)
+    grasp = multiply_poses(grasp, tf)
+
+    # Move the grasp away from the cup in the opposite direction of angle.
+    translation_distance = -0.2
+    translation_vector = (translation_distance * np.sin(angle), 0.0, translation_distance * np.cos(angle))
+    tf = Pose(translation_vector, (0.0, 0.0, 0.0, 1.0))
+    grasp = multiply_poses(grasp, tf)
+
+    return grasp
 
 
 def _main():
 
-    cup_pregrasp_transform = Pose((0.0, -0.2, 0.0), (0, np.sqrt(2) / 2, np.sqrt(2) / 2, 0))
-
-    robot, cup_id, table_id, collision_region_id = _initialize_scene()
+    robot, cup_id, table_id, other_collision_ids = _initialize_scene()
+    collision_ids = {cup_id, table_id} | other_collision_ids
     physics_client_id = robot.physics_client_id
     
     # Use the table pose as a frame of reference.
@@ -162,29 +241,26 @@ def _main():
 
     # Find target end effector pose relative to the cup.
     cup_pose = get_pose(cup_id, physics_client_id)
-    target_end_effector_pose = multiply_poses(cup_pose, cup_pregrasp_transform)
-
-    visualize_pose(target_end_effector_pose, physics_client_id)
-
-    # # TODO add back
-    # # p.removeBody(cup_id, physicsClientId=physics_client_id)
-    # # p.removeBody(table_id, physicsClientId=physics_client_id)
+    # p.removeBody(cup_id, physicsClientId=physics_client_id)  # TODO
 
     # Find target joint positions using inverse kinematics.
-    # TODO: run multiple times until collisions are avoided.
-    target_joint_positions = robot.inverse_kinematics(target_end_effector_pose, validate=True)
-    robot.set_joints(target_joint_positions)
+    max_grasp_candidates = 1000
+    rng = np.random.default_rng(0)
+    for _ in range(max_grasp_candidates):
+        import time; time.sleep(0.1)
+        candidate = _sample_grasp(cup_pose, rng)
+        target_joint_positions = collision_free_ik(robot, candidate, collision_ids)
+        if target_joint_positions is not None:
+            robot.set_joints(target_joint_positions)
+            print("Succeeded!!")
+            break
+    else:
+        print("Failed :(")
 
     # TODO: run motion planning.
 
-    for i in range(1000000000):
+    while True:
         p.stepSimulation(physicsClientId=physics_client_id)
-        # if i % 100 == 0:
-        #     pose = robot.get_end_effector_pose()
-        #     print(pose.orientation)
-        #     for i in ee_ids:
-        #         p.removeUserDebugItem(i, physicsClientId=physics_client_id)
-        #     ee_ids = visualize_pose(pose, physics_client_id)
 
 
 if __name__ == "__main__":
