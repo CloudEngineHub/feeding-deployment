1d68316e178c48e37db038a6d4b81573900a0a47
Tom Silver <tomssilver@gmail.com>
2024-08-14 17:33:19 -0400

    wip

--- pybullet-cup-manipulation/cup_manipulation.py
+++ pybullet-cup-manipulation/cup_manipulation.py
@@ -19,9 +19,10 @@ from pathlib import Path
 
 from scene import (
     create_cup_manipulation_scene,
+    CupManipulationSceneIDs,
     CupManipulationSceneDescription,
 )
-from utils import make_cup_manipulation_video
+from utils import make_cup_manipulation_video, CupManipulationTrajectory
 
 import pybullet as p
 import numpy as np
@@ -29,16 +30,14 @@ import numpy as np
 
 @lru_cache(maxsize=None)
 def generate_trajectory(
+    scene: CupManipulationSceneIDs,
     scene_description: CupManipulationSceneDescription,
     max_motion_plan_time: int = 10,
     num_grasp_waypoints: int = 5,
     seed: int = 0,
-    make_video: bool = True,
-    video_outfile: Path = Path("generated_trajectory.mp4"),
-) -> list[JointPositions]:
+) -> CupManipulationTrajectory:
 
-    physics_client_id = create_gui_connection(camera_yaw=180)
-    scene = create_cup_manipulation_scene(physics_client_id, scene_description)
+    physics_client_id = scene.physics_client_id
     robot = scene.robot
 
     collision_ids = {
@@ -48,12 +47,12 @@ def generate_trajectory(
         scene.wheelchair_id,
     }
     all_joint_positions = [robot.get_joint_positions()]
-    all_held_object_infos = [None]
+    all_held_cup_tfs = [None]
 
     # Close the fingers.
     robot.close_fingers()
     all_joint_positions.append(robot.get_joint_positions())
-    all_held_object_infos.append(None)
+    all_held_cup_tfs.append(None)
 
     # Commands will be in end effector space, but grasp planning will be in
     # finger frame space.
@@ -91,7 +90,7 @@ def generate_trajectory(
     for state in plan:
         robot.set_joints(state)
         all_joint_positions.append(state)
-        all_held_object_infos.append(None)
+        all_held_cup_tfs.append(None)
 
     # Move to grasp.
     move_amt = scene_description.cup_grasp_distance / (num_grasp_waypoints - 1)
@@ -103,12 +102,12 @@ def generate_trajectory(
         joints = end_effector_transform_to_joints(robot, tf)
         robot.set_joints(joints)
         all_joint_positions.append(joints)
-        all_held_object_infos.append(None)
+        all_held_cup_tfs.append(None)
 
     # Open the fingers to create a constraint inside the mounted holder.
     robot.open_fingers()
     all_joint_positions.append(robot.get_joint_positions())
-    all_held_object_infos.append(None)
+    all_held_cup_tfs.append(None)
 
     # Simulate grasping by faking a constraint with the held object.
     held_obj_id = scene.cup_id
@@ -127,14 +126,16 @@ def generate_trajectory(
         robot, physics_client_id, held_obj_id, base_link_to_held_obj, joints
     )
     all_joint_positions.append(joints)
-    all_held_object_infos.append((held_obj_id, base_link_to_held_obj))
+    all_held_cup_tfs.append(base_link_to_held_obj)
 
     # Move to staging pose.
     new_cup_pose = multiply_poses(
         scene_description.wheelchair_head_pose, scene_description.staging_relative_pose
     )
     cup_pose = get_pose(scene.cup_id, physics_client_id)
-    current_fingers_pose = get_link_pose(robot.robot_id, finger_frame_id, physics_client_id)
+    current_fingers_pose = get_link_pose(
+        robot.robot_id, finger_frame_id, physics_client_id
+    )
     fingers_to_cup = multiply_poses(
         cup_pose.invert(),
         current_fingers_pose,
@@ -171,19 +172,22 @@ def generate_trajectory(
 
     def _sample_fn(_current_joint_positions: JointPositions) -> JointPositions:
         del _current_joint_positions  # not used
-        return sample_joints_from_task_space_bounds(rng, robot,
-                                             min_x=min_x,
-                                             max_x=max_x,
-                                             min_y=min_y,
-                                             max_y=max_y,
-                                             min_z=min_z,
-                                             max_z=max_z,
-                                             min_roll=min_roll,
-                                             max_roll=max_roll,
-                                             min_pitch=min_pitch,
-                                             max_pitch=max_pitch,
-                                             min_yaw=min_yaw,
-                                             max_yaw=max_yaw)
+        return sample_joints_from_task_space_bounds(
+            rng,
+            robot,
+            min_x=min_x,
+            max_x=max_x,
+            min_y=min_y,
+            max_y=max_y,
+            min_z=min_z,
+            max_z=max_z,
+            min_roll=min_roll,
+            max_roll=max_roll,
+            min_pitch=min_pitch,
+            max_pitch=max_pitch,
+            min_yaw=min_yaw,
+            max_yaw=max_yaw,
+        )
 
     new_collision_ids = collision_ids - {held_obj_id}
     plan = run_smooth_motion_planning_to_pose(
@@ -204,37 +208,24 @@ def generate_trajectory(
             robot, physics_client_id, held_obj_id, base_link_to_held_obj, state
         )
         all_joint_positions.append(state)
-        all_held_object_infos.append((held_obj_id, base_link_to_held_obj))
+        all_held_cup_tfs.append(base_link_to_held_obj)
 
-    if make_video:
-        make_cup_manipulation_video(
-            scene,
-            scene_description,
-            all_joint_positions,
-            all_held_object_infos,
-            video_outfile,
-        )
-
-    p.disconnect(physics_client_id)
-
-    return all_joint_positions, all_held_object_infos
+    return CupManipulationTrajectory(all_joint_positions, all_held_cup_tfs)
 
 
 if __name__ == "__main__":
     scene_description = CupManipulationSceneDescription()
+    physics_client_id = create_gui_connection(camera_yaw=180)
+    scene = create_cup_manipulation_scene(physics_client_id, scene_description)
+    traj = generate_trajectory(
+        scene, scene_description, seed=0, max_motion_plan_time=20
+    )
+    video_outfile = Path("generated_trajectory.mp4")
+    make_cup_manipulation_video(
+        scene,
+        scene_description,
+        traj,
+        video_outfile,
+    )
+    p.disconnect(physics_client_id)
 
-    # scene_rotation = tuple(
-    #     p.getQuaternionFromEuler((np.pi / 8, -np.pi / 4, -np.pi / 2))
-    # )
-    # scene_description = scene_description.rotate_about_point(
-    #     (0.0, 0.0, 0.0), scene_rotation
-    # )
-
-    generate_trajectory(scene_description, seed=0, max_motion_plan_time=20)
-
-    # from scipy.spatial.transform import Rotation
-
-    # quat = tuple(Rotation.from_euler('xyz', [0, 0, 90], degrees=True).as_quat())
-    # robot_base_pose = Pose((0.0, 0.0, 0.0), quat)
-    # robot_initial_joints = (2.80374963034063, 5.737339549099201, 3.3692055751078134, 2.2763574480856223, 3.002470982456817, 1.2413268146451608, 1.505290153988054, 0.5973799438476562, 0.5973799438476562)
-    # all_joint_positions = generate_trajectory(robot_initial_joints, robot_base_pose)
--- pybullet-cup-manipulation/utils.py
+++ pybullet-cup-manipulation/utils.py
@@ -7,6 +7,7 @@ from pybullet_helpers.geometry import Pose
 from pybullet_helpers.joint import (
     JointPositions,
 )
+from dataclasses import dataclass
 from pybullet_helpers.camera import capture_superimposed_image
 import imageio.v2 as iio
 from pathlib import Path
@@ -16,30 +17,38 @@ from scene import (
     CupManipulationSceneIDs,
 )
 
-import pybullet as p
 
+@dataclass
+class CupManipulationTrajectory:
+    """A trajectory for cup manipulation."""
+
+    joint_states: list[JointPositions]
+    held_cup_transforms: list[Pose | None]
+
+    def __post_init__(self) -> None:
+        assert len(self.joint_states) == len(self.held_cup_transforms)
 
 
 def make_cup_manipulation_video(
     scene: CupManipulationSceneIDs,
     scene_description: CupManipulationSceneDescription,
-    joint_states: list[JointPositions],
-    held_object_info: list[tuple[int, Pose] | None],
+    traj: CupManipulationTrajectory,
     outfile: Path,
     fps: int = 20,
 ) -> None:
     """Make a video for a simulated cup manipulation plan."""
     scene.reset(scene_description)
     imgs = []
-    for joint_state, info in zip(joint_states, held_object_info, strict=True):
-        if info is None:
+    for joint_state, base_link_to_held_obj in zip(
+        traj.joint_states, traj.held_cup_transforms, strict=True
+    ):
+        if base_link_to_held_obj is None:
             scene.robot.set_joints(joint_state)
         else:
-            held_obj_id, base_link_to_held_obj = info
             set_robot_joints_with_held_object(
                 scene.robot,
                 scene.physics_client_id,
-                held_obj_id,
+                scene.cup_id,
                 base_link_to_held_obj,
                 joint_state,
             )
