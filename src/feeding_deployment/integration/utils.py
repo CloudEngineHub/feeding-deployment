11b19b79a9fb72703c0130746a770ff8ba49fe41
RKJenamani <rajatkj11@gmail.com>
2024-08-19 15:44:58 -0400

    fix

--- src/feeding_deployment/integration/utils.py
+++ src/feeding_deployment/integration/utils.py
@@ -17,16 +17,22 @@ def cup_manipulation_trajectory_to_kinova_commands(
     """The Kinova controller expects arm joints and gripper values."""
     cmds = []
     last_gripper: float | None = None
+    current_trajectory = []
     for joint_state in traj.joint_states:
         assert len(joint_state) == 9  # making assumptions about Kinova
         arm = np.array(joint_state[:7])
         assert np.isclose(joint_state[7], joint_state[8])
         gripper = joint_state[8]
         if last_gripper is None or not np.isclose(gripper, last_gripper):
+            if current_trajectory:
+                cmds.append(JointTrajectoryCommand(current_trajectory))
+                current_trajectory = []
             if last_gripper is None or last_gripper >= 0:
                 cmds.append(CloseGripperCommand())
             else:
                 cmds.append(OpenGripperCommand())
+        current_trajectory.append(arm)
         last_gripper = gripper
-        cmds.append(JointTrajectoryCommand(arm))
+    if current_trajectory:
+        cmds.append(JointTrajectoryCommand(current_trajectory))
     return cmds
