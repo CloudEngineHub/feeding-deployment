27423e3d486b73d76c5f171fde206056216e7d3c
RKJenamani <rajatkj11@gmail.com>
2025-01-08 15:19:53 -0500

    updated descriptions (again)

--- src/feeding_deployment/actions/emulate_transfer.py
+++ src/feeding_deployment/actions/emulate_transfer.py
@@ -29,7 +29,7 @@ from feeding_deployment.actions.base import (
 from feeding_deployment.actions.feel_the_bite.outside_mouth_transfer import OutsideMouthTransfer
 
 class EmulateTransferHLA(HighLevelAction):
-    """Emulate transfer by bringing the robot infront of the user's mouth."""
+    """Emulate transfer by bringing the empty gripper infront of the user's mouth."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
